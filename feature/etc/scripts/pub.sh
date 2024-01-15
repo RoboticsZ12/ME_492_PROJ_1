926be51f8baaef83eac2f46b868253bb38f6bf7d
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-15 11:03:37 -0600

    Update pub.sh

--- feature/etc/scripts/pub.sh
+++ feature/etc/scripts/pub.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+# BASH SCRIPT: FUNCTION PARAMETER
 
-# Bash script using echo
-
-echo "Welcome to my Bash script!"
-echo "This script will display some information."
+function parameter()
+{
+# opt = option, parameter one
+opt = "$1"
+# topic_name = name of topic for publisher, parameter 2
+topic_name = "$2"
+# mas_type = published msg type, parameter 3
+msg_type = "$3"
+# msg is value of msg, parameter 4
+msg = "$4"
+}
