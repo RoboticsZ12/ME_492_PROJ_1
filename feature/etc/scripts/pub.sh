ba89b906c00bcfb6a66f2414dd250c6ac52ee2d6
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-15 11:14:52 -0600

    Update pub.sh

--- feature/etc/scripts/pub.sh
+++ feature/etc/scripts/pub.sh
@@ -4,11 +4,11 @@
 function parameter()
 {
 # opt = option, parameter one
-opt = "$1"
+opt ="$1"
 # topic_name = name of topic for publisher, parameter 2
-topic_name = "$2"
+topic_name ="$2"
 # mas_type = published msg type, parameter 3
-msg_type = "$3"
+msg_type ="$3"
 # msg is value of msg, parameter 4
-msg = "$4"
+msg ="$4"
 }
