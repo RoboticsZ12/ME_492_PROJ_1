6539d12cd60226a3cf62687073eaba1bce551e0a
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-15 11:27:50 -0600

    Update sub.sh

--- feature/etc/scripts/sub.sh
+++ feature/etc/scripts/sub.sh
@@ -1,3 +1,11 @@
-# BASH FILE: ECHO TO THE TERMINAL
+# BASH FILE: sub.sh
+#ECHO TO THE TERMINAL
 
-
+function topic()
+{
+  source pub.sh
+  parameter "opt" "topic_name" "msg_type" "msg"
+  echo "The topic_name: \n"
+  read topic_name
+  echo "The topic name is: $topic_name \n"
+}
