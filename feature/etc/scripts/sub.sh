2cffa4a8535361638c9ee2bae2120fdb4068a362
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-15 15:43:30 -0600

    Update sub.sh

--- feature/etc/scripts/sub.sh
+++ feature/etc/scripts/sub.sh
@@ -1,12 +1,29 @@
 # BASH FILE: sub.sh
 #ECHO TO THE TERMINAL
 
-function topic()
+#--------------------------------------------------
+# THIS FUNCTION WILL ACQUIRE THE <topic_name>, and 
+# <file_name> from simple "read" USER INPUTS. 
+# THESE ARGUMENTS HAVE CREATED IN <pub.sh>
+#--------------------------------------------------
+
+function argument_topic()
 {
+# CALLING <pub.sh> to assing arguments.
   source pub.sh
   parameter "opt" "topic_name" "msg_type" "msg"
-  echo "The topic_name: \n"
+
+# ASSIGNING topic_name argument
+  echo "Enter the topic_name: "
   read topic_name
-  echo "The topic name is: $topic_name \n"
+  echo "The name is: $topic_name \n"
+
+# ASSIGNING file_name argument
+  echo "Enter the file_name: "
+  read file_name
+  echo "The name is: $file_name \n"
+
+# SETTING PUBLISHING RATE
+  
 }
 echo "hello"
