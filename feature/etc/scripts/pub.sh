f5e879238e8e5ca6b132b6fcd17ee0ba2a1d4acf
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-15 12:24:43 -0600

    Update pub.sh

--- feature/etc/scripts/pub.sh
+++ feature/etc/scripts/pub.sh
@@ -1,6 +1,12 @@
 # BASH SCRIPT: pub.sh
 #FUNCTION PARAMETER
 
+#---------------------------------------------
+# THIS FUNCTION WILL OUTPUT WHICH PARAMETER 
+# THE USER INPUTS. 
+# THIS FUNCTION WILL CHECK WHICH PARAMETER THE 
+# THE USER WISHES TO OUTPUT. 
+#----------------------------------------------
 function parameter()
 {
 # opt = option, parameter one
@@ -12,9 +18,24 @@ msg_type ="$3"
 # msg is value of msg, parameter 4
 msg ="$4"
 
+# -z means that the string returns a zero string
+# if you want to output the amount of parameters intputted, use {$#}
+if[ "$opt" == -z && "$topic_name" == -z && "$msg_type" == -z &&  "$msg" == -z ]; then
+  echo "The number of arguments inputted is: $#"
+  echo "The parameters have no inputs."
+  
+else[ "$opt" != -z || "$topic_name" != -z || "$msg_type" != -z ||  "$msg" != -z ]; then
+  echo "Parameter: \n"
+  echo "opt: $opt \n"
+  echo "topic_name: $topic_name \n"
+  echo "msg_type: $msg_type \n"
+  echo "msg: $msg \n"
 
-
-
+exit 1
+# fi indicates the end of the if/else 
+fi
 }
 echo "jack"
+
+# This line is used to make this function accessible to outside functions.
 export -f paramter
