64e265357afee035f240bcade3436621a4a7c6f3
RoboticsZ12 <142946153+RoboticsZ12@users.noreply.github.com>
2024-01-16 21:53:11 -0600

    Update sub.sh

--- feature/etc/scripts/sub.sh
+++ feature/etc/scripts/sub.sh
@@ -7,12 +7,13 @@
 # THESE ARGUMENTS HAVE CREATED IN <pub.sh>
 #--------------------------------------------------
 
-function argument_topic()
-{
+
 # CALLING <pub.sh> to assing arguments.
   source pub.sh
-  parameter "opt" "topic_name" "msg_type" "msg"
 
+
+function argument_topic()
+{
 # ASSIGNING topic_name argument
   echo "Enter the topic_name: "
   read topic_name
@@ -25,18 +26,15 @@ function argument_topic()
 
   if[$file_name != $file_name.cvs]; then 
     echo "Please enter a file name that ends in .cvs"
-  exit 1
+    exit 1
   fi
-}
 
-function topic_cvs($topic_name)
-{
 # SETTING PUBLISHING RATE TO THE TERMINAL
   echo "At what rate would you like to publish the data? "
   read pub_rate
   echo "Pub rate has been set to: $pub_rate \n"
 
-# RETRIEVING TOPIC NAMES
-echo "$topic_name"
+# CALLING FUNCTION "parameter" FROM FILE pub.sH
+  publish
 }
 echo "hello"
