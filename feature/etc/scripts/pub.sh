16216b7aa79fddda6ecde93039e3b26981af17b8
RoboticsZ12 <zechariahgeorgian@gmail.com>
2024-01-18 11:15:39 -0600

    comment out if statement

--- feature/etc/scripts/pub.sh
+++ feature/etc/scripts/pub.sh
@@ -1,4 +1,5 @@
-# BASH SCRIPT: pub.sh
+#! /bin/bash
+#BASH SCRIPT: pub.sh
 #FUNCTION PARAMETER
 
 #-------------------------------------------
@@ -17,34 +18,36 @@ msg ="$4"
 # -z means that the string returns a zero string
 # if you want to output the amount of parameters intputted, use {$#}
 # {$} in the below if else is used to reference the previous parameters described.
-if[ -z "$opt" && -z "$topic_name" && -z "$msg_type" &&  -z "$msg" ]; then
-  echo "The number of arguments inputted is: $#"
-  echo "The parameters have no inputs."
+#if[ -z "$opt" && -z "$topic_name" && -z "$msg_type" &&  -z "$msg" ]; then
+ # echo "The number of arguments inputted is: $#"
+  #echo "The parameters have no inputs."
   
-elif[ -n "$opt" || -n "$topic_name" || -n "$msg_type" ||  -n "$msg" ]; then
-  echo "Parameter: \n"
-  echo "opt: $opt \n"
-  echo "topic_name: $topic_name \n"
-  echo "msg_type: $msg_type \n"
-  echo "msg: $msg \n"
+#elif[ -n "$opt" || -n "$topic_name" || -n "$msg_type" ||  -n "$msg" ]; then
+ # echo "Parameter: \n"
+  #echo "opt: $opt \n"
+  #echo "topic_name: $topic_name \n"
+  #echo "msg_type: $msg_type \n"
+  #echo "msg: $msg \n"
 exit 1
 # fi indicates the end of the if/else 
 fi
 
-  echo "Publishing now. Press any key to stop. \n"
-  read stop_input
-  sleep 1
+  #echo "Publishing now. Press any key to stop. \n"
+  #read stop_input
+  #sleep 1
 
 # THIS LOOP WILL RUN AND PUBLISH ALL ARGUMENTS WHILE, -Z, THERE IS NO STRING DETECTED
-  while [ -z $stop_input ]; do 
-    echo "pub: $opt $topic_name $msg_type $msg \n"
-  if[ -n $stop_input ]; then 
-    echo "Hope you enjoyed the loop."
-    break
-  fi
+  #while [ -z $stop_input ]; do 
+   # echo "pub: $opt $topic_name $msg_type $msg \n"
+  #if[ -n $stop_input ]; then 
+   # echo "Hope you enjoyed the loop."
+   # break
+  #fi
 # PRINT OUT ECHO ABOVE EVERY 1 SECOND. 
   sleep 1
 
 echo $topic_name
 ros2 topic pub $opt $topic_name $msg_type $msg  
 }
+
+parameter()
